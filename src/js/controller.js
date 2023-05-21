6e4cf8f83a5f960f9009c9e8ebe6282784acde06
LvMaoGui <1754628156@qq.com>
2023-05-21 22:44:37 +0800

    根据获取到接口数据，生成html脚本渲染到页面

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,10 @@
+// import icons from '../img/icons.svg' // parcel v1
+import icons from 'url:../img/icons.svg' // parcel v2
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -12,29 +19,139 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const showRecipe = async function() {
-  try{
-    const result = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=<insert your key>')
+const renderSpinner = (parentEl) => {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>
+  `
+  // 先清空内容
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup)
+}
+
+const showRecipe = async function () {
+  try {
+    // 1.加载配方
+    renderSpinner(recipeContainer)
+
+    const result = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=<insert your key>'
+    );
 
     const data = await result.json();
 
-    if(!result.ok) throw new Error(`${data.message}`)
-    console.log(data)
+    if (!result.ok) throw new Error(`${data.message}`);
 
-    let {recipe} = data.data
+    let { recipe } = data.data;
     recipe = {
-      id:recipe.id,
-      title:recipe.title,
+      id: recipe.id,
+      title: recipe.title,
       publisher: recipe.publisher,
       sourceUrl: recipe.source_url,
       image: recipe.image_url,
       servings: recipe.servings,
       cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients
-    }
-  }catch(error){
-    console.error(error.message)
-  }
-}
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
 
-showRecipe()
+    // 2.渲染配方
+    const markup = `
+      <figure class="recipe__fig">
+        <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
+        <h1 class="${recipe.title}">
+          <span>Pasta with tomato cream sauce</span>
+        </h1>
+      </figure>
+
+      <div class="recipe__details">
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-clock"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+          <span class="recipe__info-text">minutes</span>
+        </div>
+        <div class="recipe__info">
+          <svg class="recipe__info-icon">
+            <use href="${icons}#icon-users"></use>
+          </svg>
+          <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+          <span class="recipe__info-text">servings</span>
+
+          <div class="recipe__info-buttons">
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${icons}#icon-minus-circle"></use>
+              </svg>
+            </button>
+            <button class="btn--tiny btn--increase-servings">
+              <svg>
+                <use href="${icons}#icon-plus-circle"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div class="recipe__user-generated">
+          <svg>
+            <use href="${icons}#icon-user"></use>
+          </svg>
+        </div>
+        <button class="btn--round">
+          <svg class="">
+            <use href="${icons}#icon-bookmark-fill"></use>
+          </svg>
+        </button>
+      </div>
+
+      <div class="recipe__ingredients">
+        <h2 class="heading--2">Recipe ingredients</h2>
+        <ul class="recipe__ingredient-list">
+          ${recipe.ingredients.map(ingredient => 
+            `<li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="${icons}#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">${ingredient.quantity}</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${ingredient.unit}</span>
+                ${ingredient.description}
+              </div>
+            </li>
+          `)}
+        </ul>
+      </div>
+
+      <div class="recipe__directions">
+        <h2 class="heading--2">How to cook it</h2>
+        <p class="recipe__directions-text">
+          This recipe was carefully designed and tested by
+          <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+          directions at their website.
+        </p>
+        <a
+          class="btn--small recipe__btn"
+          href="${recipe.sourceUrl}"
+          target="_blank"
+        >
+          <span>Directions</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </div>
+    `;
+    // 先将容器中的内容清空
+    recipeContainer.innerHTML = ''
+    // 将html脚本插入容器中进行渲染
+    recipeContainer.insertAdjacentHTML("afterbegin", markup)
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+showRecipe();
